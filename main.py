56f73a27bfe1f50e09629e8fd22e47c2a6b53d7d
corbin <stramapatryk@gmail.com>
2022-11-27 23:10:11 +0100

    Completed check_result, get_player_mark, get_computer_mark. Create function terminal_game_mode.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from enum import Enum
+import re
+import random
 
 
 class Players(Enum):
     X = 1
     O = 2
+
+
+class Results(Enum):
+    running = 0
+    X_won = 1
+    draw = 2
+    O_won = 3
 
 
 class Map:
@@ -75,11 +84,16 @@ class Map:
     def check_result(self):
         # find if game ended
 
-        x_player_win = False
-        y_player_win = False
-        draw = False
+        if self.check_for(Players.X):
+            return Results.X_won
 
-        return False
+        if self.check_for(Players.O):
+            return Results.O_won
+
+        if self.is_full():
+            return Results.draw
+
+        return Results.running
 
     def show_in_terminal(self):
         # print in terminal
@@ -134,8 +148,18 @@ class Window:
 def get_player_mark():
     # get console input
 
-    row = 1
-    column = 2
+    column = input("Please Enter column number (1-3): ")
+    while not re.match("^[1-3]$", column):
+        print("Incorrect input.")
+        column = input("Please Enter column number (1-3): ")
+
+    row = input("Please Enter row number (1-3): ")
+    while not re.match("^[1-3]$", row):
+        print("Incorrect input.")
+        row = input("Please Enter row number (1-3): ")
+
+    row = int(row) - 1
+    column = int(column) - 1
 
     return row, column
 
@@ -143,18 +167,54 @@ def get_player_mark():
 def get_computer_mark():
     # randomize computer input
 
-    row = 0
-    column = 1
+    row = random.randint(0, 2)
+    column = random.randint(0, 2)
 
     return row, column
 
 
+def terminal_game_mode():
+    # Map init
+    game_map = Map()
+    results = game_map.check_result()
+
+    # Game Loop
+    while results is results.running:
+        # Show map
+        game_map.show_in_terminal()
+
+        # Player move
+        while not game_map.mark_place(get_player_mark(), Players.X):
+            print("Space is occupied")
+
+        results = game_map.check_result()
+        if results is Results.X_won:
+            break
+
+        # Computer move
+        while not game_map.mark_place(get_computer_mark(), Players.O):
+            pass
+
+        game_map.check_result()
+
+    # End Screen
+    game_map.show_in_terminal()
+
+    if results is Results.X_won:
+        print("Congratulation! You win")
+
+    if results is Results.draw:
+        print("Draw")
+
+    if results is Results.O_won:
+        print("Oh no. Computer won. You can do it")
+
+
 def main():
     print("Hello")
-    game_map = Map()
-    game_map.show_in_terminal()
-    game_map.mark_place(get_player_mark(), Players.O)
-    game_map.show_in_terminal()
+
+    terminal_game_mode()
 
 
+random.seed()
 main()

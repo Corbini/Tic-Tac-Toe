b08e2448108bb1a539909f448b008cc5ee2c9eea
corbin <stramapatryk@gmail.com>
2022-11-27 18:29:18 +0100

    Init commit. Planned functions. Added README.txt which include roadmap
    Completed mark_place, check_for, is_full, check_result, show_in_terminal

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
+from enum import Enum
+
+
+class Players(Enum):
+    X = 1
+    O = 2
 
 
 class Map:
     def __init__(self):
-        self.structure= [[0 for x in range(3)] for y in range(3)]
+        self.structure = [[0 for x in range(3)] for y in range(3)]
         # 0 is empty, 1 is X, 2 is O
 
-    def mark_place(self, place):
+    def mark_place(self, place, player: Players):
         # Checking if place is empty
 
-        print(place[0])
-        print(place[1])
+        if self.structure[place[0]][place[1]] == 0:
+            self.structure[place[0]][place[1]] = player.value
+            return True
 
-        return True
+        return False
 
-    def check_for(self, player):
-        #win check for player
+    def check_for(self, player: Players):
+        # win check for player
+
+        # line up-down
+        for column in range(0, 2):
+            flag = True
+            for row in range(0, 2):
+                if self.structure[column][row] != player.value:
+                    flag = False
+                    break
+            if flag is True:
+                return True
+
+        # line left-right
+        for row in range(0, 2):
+            flag = True
+            for column in range(0, 2):
+                if self.structure[column][row] != player.value:
+                    flag = False
+                    break
+            if flag is True:
+                return True
+
+        # crosses
+        flag = True
+        for diagonal in range(0, 2):
+            if self.structure[diagonal][diagonal] != player.value:
+                flag = False
+                break
+        if flag is True:
+            return True
+
+        flag = True
+        for diagonal in range(0, 2):
+            if self.structure[diagonal][2 - diagonal] != player.value:
+                flag = False
+                break
+        if flag is True:
+            return True
 
         return False
 
     def is_full(self):
-        #check for new move
+        # check for new move
 
-        return False
+        for row in self.structure:
+            for space in row:
+                if space == 0:
+                    return False
+
+        return True
 
     def check_result(self):
         # find if game ended
@@ -35,7 +84,20 @@ class Map:
     def show_in_terminal(self):
         # print in terminal
 
-        pass
+        text = "map:\n"
+
+        for row in self.structure:
+            for space in row:
+                if space == 1:
+                    text += "X "
+
+                elif space == 2:
+                    text += "O "
+                else:
+                    text += "_ "
+            text += "\n"
+
+        print(text)
 
 
 class Window:
@@ -91,7 +153,8 @@ def main():
     print("Hello")
     game_map = Map()
     game_map.show_in_terminal()
-    game_map.mark_place(get_player_mark())
+    game_map.mark_place(get_player_mark(), Players.O)
+    game_map.show_in_terminal()
 
 
 main()

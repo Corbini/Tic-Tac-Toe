64fc91e920cbf8d65a5252ea7c1404c2bcce545b
corbin <stramapatryk@gmail.com>
2022-11-28 23:10:18 +0100

    Completed check_result, get_player_mark, get_computer_mark. Create function terminal_game_mode. Added pyqt lib. Complete clear, __init__ in Window class. Added add_button function. Completed start_window

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import random
 
 # window lib
 import sys
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QMainWindow, QPushButton, QVBoxLayout, QGridLayout
+from PyQt6.QtGui import QIcon, QFont, QPixmap, QMovie, QRegion
+from PyQt6.QtCore import Qt, QSize
 
 
 class Players(Enum):
@@ -123,34 +124,68 @@ class Map:
         print(text)
 
 
+class Button(QLabel):
+    def __init__(self, image, text):
+        super(Button, self).__init__()
+        self.setFixedSize(image.size())
+        self.setPixmap(image)
+        pass
+
+
 class Window(QMainWindow):
     def __init__(self, qapp):
         self.app = qapp
         super().__init__()
         self.setWindowTitle("Tic Tac Toe")
+        self.setWindowIcon(QIcon('images/Logo.png'))
+        self.background = QLabel()
+        self.setCentralWidget(self.background)
+        self.grid = QGridLayout()
+        self.background.setLayout(self.grid)
         self.show()
         self.start_window()
 
     def game_window(self, game_map):
         # Create game stage
         self.clear()
+
+        # Load Images
+        game_window_png = QPixmap('images/Game_Window.png')
+
+        # Set Background
+        background = QLabel(self)
+        background.setPixmap(game_window_png)
+        self.setCentralWidget(background)
+
         pass
 
     def clear(self):
         children = self.children()
-        for child in children[1:]:
+        for child in children[2:]:
             child.deleteLater()
 
+    def add_button(self, button_png, text, func, x, y):
+        button = Button(button_png, text)
+        button.mousePressEvent = func
+        button.move(x, y)
+        self.layout().addWidget(button)
+
     def start_window(self):
+        # Clearing window
         self.clear()
+
         # Create start stage
+        menu_window_png = QPixmap('images/Menu_Window.png')
+        button_png = QPixmap('images/Button.png')
 
-        button = QPushButton("New game")
-        button.setCheckable(True)
-        button.clicked.connect(self.game_window)
-        self.setCentralWidget(button)
+        # Adding background
+        self.background.setPixmap(menu_window_png)
 
-        self.app.exec()
+        # Adding start button
+        self.add_button(button_png, "Start Game", self.game_window, 490, 580)
+
+        # Adding quit button
+        self.add_button(button_png, "Quit", self.close, 490, 770)
 
     def end_window(self):
         # Create end stage
@@ -237,9 +272,7 @@ def main():
 
     app = QApplication(sys.argv)
     game = Window(app)
-    #game.start_window()
-
-    terminal_game_mode()
+    app.exec()
 
 
 random.seed()
